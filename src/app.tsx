e79090df7a14e01a3db83692fb9c20d692654396
Matheus Oliveira <matheussilvaoliveira@hotmail.com>
2026-05-25 15:24:15 -0400

    adjusting favicon and logo

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, useState } from 'react'
-import logo from './assets/logo-nlw-expert.svg'
+import logo from './assets/logo.png'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
@@ -58,7 +58,7 @@ export function App() {
 
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
-      <img src={logo} alt="NLW Expert" />
+      <img src={logo} alt="NLW Expert" className="h-72 w-auto" />
 
       <form className="w-full">
         <input 
