7126ad589379eb26a62b8e3d039aff429e0920cd
Matheus Oliveira <matheussilvaoliveira@hotmail.com>
2026-05-25 10:51:26 -0400

    fix: refacing code

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -103,7 +103,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
             <X className="size-5" />
           </Dialog.Close>
 
-          <form className="flex-1 flex flex-col">
+          <form className="flex-1 flex flex-col overflow-y-auto">
             <div className="flex flex-1 flex-col gap-3 p-5">
               <span className="text-sm font-medium text-slate-300">
                 Adicionar nota
@@ -151,7 +151,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
               <button
                 type="button"
                 onClick={handleSaveNote}
-                className="w-full bg-lime-400 py-4 pb-safe text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+                className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
               >
                 Salvar nota
               </button>
